8d67a9d0e604174d0e3f7c62de36eb44b7a30883
michael <mpgarate@gmail.com>
2017-11-18 15:49:55 -0500

    tabs to spaces

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,36 +6,36 @@ use std::io::Read;
 use std::io::Write;
 
 fn main() {
-	let pool = ThreadPool::new(15);
+    let pool = ThreadPool::new(15);
 
-	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-	// accept connections and process them serially
-	for stream in listener.incoming() {
-		pool.execute(|| {
-			handle_client(stream.unwrap());
-		});
-	}
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        pool.execute(|| {
+            handle_client(stream.unwrap());
+        });
+    }
 }
 
 fn read_http(stream: &TcpStream) -> String {
-	stream.bytes()
-		.map(|r| r.unwrap_or('�' as u8) as char)
-		.filter(|c| c != &'\r')
-		.take_while(|c| c != &'\n')
-		.collect()
+    stream.bytes()
+        .map(|r| r.unwrap_or('�' as u8) as char)
+        .filter(|c| c != &'\r')
+        .take_while(|c| c != &'\n')
+        .collect()
 }
 
 fn handle_client(mut stream: TcpStream) {
-	loop {
-		let s = read_http(&stream);
-		println!("{:?}", s);
+    loop {
+        let s = read_http(&stream);
+        println!("{:?}", s);
 
-		if s.is_empty() {
-			break;
-		}
-	}
+        if s.is_empty() {
+            break;
+        }
+    }
 
-	let response = "HTTP/1.1 200 OK
+    let response = "HTTP/1.1 200 OK
 Date: Mon, 27 Jul 2009 12:28:53 GMT
 Server: Apache
 Last-Modified: Wed, 18 Nov 2017 00:00:00 GMT
@@ -46,6 +46,6 @@ Content-Type: text/plain
 
 Hello World! My payload includes a trailing CRLF.\r\n".as_bytes();
 
-	let _ = stream.write(response);
+    let _ = stream.write(response);
 }
 
